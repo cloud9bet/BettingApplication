e4268bfac559d30fdf3d4107a8c8ac4e96ffdf67
Hiru <hirusigan@gmail.com>
2025-10-14 08:00:27 +0200

    tilføjet alt nuværende logic

---
+++ BettingSite/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import SettingsBtn from "../components/SettingsBtn"
+
+function HomePage() {
+
+
+  return (
+  <div>
+    <p>Hey from homepage</p>
+      <div>
+      <SettingsBtn/>
+      </div>
+  </div>
+  )
+}
+
+export default HomePage
---
+++ BettingSite/src/pages/SettingsPage.jsx
@@ -0,0 +1,16 @@
+import SettingsBtn from "../components/SettingsBtn"
+
+function HomePage() {
+
+
+  return (
+  <div>
+    <p>Hey from Settingspage</p>
+      <div>
+      <SettingsBtn/>
+      </div>
+  </div>
+  )
+}
+
+export default HomePage
