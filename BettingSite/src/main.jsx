1a085093c2df63e06696d7f4123a851ad5b5f90b
andreasfrost1004 <dk.andreasfrost@gmail.com>
2025-10-18 18:07:07 +0200

    hashrouting for 404 reload fix

--- BettingSite/src/main.jsx
+++ BettingSite/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './App.jsx'
 import { BalanceContextProvider } from './Context/BalanceContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/BettingApplication">
-    <BalanceContextProvider>
-    <App />
-    </BalanceContextProvider>
-    </BrowserRouter>
-  </StrictMode>,
+    <HashRouter>
+      <BalanceContextProvider>
+        <App />
+      </BalanceContextProvider>
+    </HashRouter>
+  </StrictMode>
 )
