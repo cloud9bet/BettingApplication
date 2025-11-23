7425b54958f4584dd43a850a18b58494d5331a42
malaravan velalagan <au717615@au.uni.dk>
2025-11-23 11:59:25 +0100

    Filer der ikke bruges fjernet i slotmachine

--- BettingSite/src/components/SlotMachineGame/gameComponents/InputNumber.jsx
+++ BettingSite/src/components/SlotMachineGame/gameComponents/InputNumber.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "../gameStyles/InputNumber.css";
 // ændre max senere
-export default function InputNumber({ value, /*min = 1, max = 5000,*/ disabled, onChange}) {
+export default function InputNumber({ value, disabled, onChange}) {
     return (
       < input
       className = "slot-input"  
       type = "text"
       value = {value}
-      /*min = {min}
-      max = {max}*/
       disabled = {disabled}
       onChange={(e) => onChange(Number(e.target.value) || 0)}
       />  
--- BettingSite/src/components/SlotMachineGame/gameComponents/constants.js
+++ BettingSite/src/components/SlotMachineGame/gameComponents/constants.js
@@ -10,15 +10,14 @@ import { SiBitcoinsv } from "react-icons/si";
 */
 
 // Available symbols
-//export const SYMBOLS = ["🍒", "🍀", "9️⃣", "🪙"];
 export const SYMBOLS_KEYS = ["diamond", "clover", "nine", "coin"];
 
 
 
 export const SYMBOL_COMPONENTS = {
-  diamond: GiCutDiamond  ,
-  clover: GiClover,       // fx 4-leaf clover vibe
-  nine: PiNumberCircleNineDuotone,     // eller et andet "9" ikon hvis du vil
+  diamond: GiCutDiamond,
+  clover: GiClover,       
+  nine: PiNumberCircleNineDuotone,     
   coin: SiBitcoinsv,
 };
 
@@ -29,33 +28,3 @@ export const BACKEND_SYMBOL_MAP = {
   "C": "coin",
 };
 
-
-
-// Payout table (per symbol, per match type)
-/*export const PAYOUTS = {
-    "🍒": { horizontal3: 15, vertical3: 16, diagonal3: 12}, // hvordan skal jeg fortå dette
-    "🍀": { horizontal3: 12, vertical3: 15, diagonal3: 14},
-    "9️⃣": { horizontal3: 50, vertical3: 60, diagonal3: 55},
-    "🪙": { horizontal3: 25, vertical3: 30, diagonal3: 28},
-};*/
-
-
-
-// Payline definitions with explicit type
-export const PAYLINES = [
-    
-    // Horizontal linjer
-    { coords: [[0, 0], [0, 1], [0, 2]], type: "horizontal3" },
-    { coords: [[1, 0], [1, 1], [1, 2]], type: "horizontal3" },
-    { coords: [[2, 0], [2, 1], [2, 2]], type: "horizontal3" },
-
-    // Vertical linjer
-    { coords: [[0, 0], [1, 0], [2, 0]], type: "vertical3" },
-    { coords: [[0, 1], [1, 1], [2, 1]], type: "vertical3" },
-    { coords: [[0, 2], [1, 2], [2, 2]], type: "vertical3" },
-
-    // Diagonal linjer
-    { coords: [[0, 0], [1, 1], [2, 2]], type: "diagonal3" },
-    { coords: [[0, 2], [1, 1], [2, 0]], type: "diagonal3" },
-
-];
